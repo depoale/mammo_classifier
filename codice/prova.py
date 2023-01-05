227c902dcb3755b03070d7fbe1247cfbda47bdd4
depoale <94780408+depoale@users.noreply.github.com>
2023-01-05 19:47:57 +0100

    first trial kinda shitty

--- codice/prova.py
+++ codice/prova.py
@@ -2,9 +2,12 @@ import os
 import sys
 from matplotlib import pyplot as plt
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout, Rescaling, Input
 import keras
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau
+from keras.optimizers import Adam
+from keras.metrics import Precision, Recall, BinaryAccuracy
+import time
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 sys.path.insert(
@@ -12,7 +15,6 @@ sys.path.insert(
 ) 
 from keras.utils import image_dataset_from_directory
 
-import os
 from PIL import Image
 
 """ 
@@ -24,52 +26,86 @@ for file in os.listdir('datasets/Mammography_micro/Test/0'):
         with Image.open(os.path.join('datasets/Mammography_micro/Test/0',file)) as im:
             im.save(new_file)
  """
-data = image_dataset_from_directory('data_png/Train')
-print('lunghezza',len(data))
-data_iterator = data.as_numpy_iterator()
-batch = data_iterator.next()
-print('lunghezza',len(data))
-fig, ax = plt.subplots(ncols=4, figsize=(5,5))
-for idx, img in enumerate(batch[0][:4]):
-    ax[idx].imshow(img.astype(int))
-    ax[idx].title.set_text(batch[1][idx])
-#scale data
-data = data.map(lambda x,y: (x/255, y))
-data.as_numpy_iterator().next()
 
-train_size = int(len(data)*.7)
-val_size = int(len(data)*.2)
-test_size = int(len(data)*.1)
+batch_size = 12
+img_height = 60
+img_width = 60
 
-train = data.take(train_size)
-val = data.skip(train_size).take(val_size)
-test = data.skip(train_size+val_size).take(test_size)
+train = image_dataset_from_directory(
+  'data_png/Train',
+  validation_split=0.3,
+  subset="training",
+  seed=123, color_mode='grayscale',
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+val = image_dataset_from_directory(
+  'data_png/Train',
+  validation_split=0.3,
+  subset="validation",
+  seed=123,
+  color_mode='grayscale',
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+test = image_dataset_from_directory(
+  'data_png/Test',
+  color_mode='grayscale',
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
 
 #model
 model = Sequential()
-model.add(Conv2D(16, (3,3), 1, activation='relu', input_shape=(256,256,3)))
+model.add(Input(shape=(60,60,1)))
+#this layer normalizes the grayscale values from [0,255] to [0,1]
+model.add(Rescaling(1./255))
+model.add(Conv2D(32, kernel_size=3, activation='relu'))
 model.add(MaxPooling2D())
-model.add(Conv2D(32, (3,3), 1, activation='relu'))
+model.add(Conv2D(32, kernel_size=3, activation='relu'))
 model.add(MaxPooling2D())
-model.add(Conv2D(16, (3,3), 1, activation='relu'))
+model.add(Conv2D(32, kernel_size=3, activation='relu'))
 model.add(MaxPooling2D())
 model.add(Flatten())
-model.add(Dense(256, activation='relu'))
+model.add(Dense(60,activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
-model.compile('adam', loss=keras.losses.BinaryCrossentropy(), metrics=['accuracy'])
+model.compile(Adam(learning_rate=1e-4), loss=keras.losses.BinaryCrossentropy(), metrics=['accuracy'])
 model.summary()
 
 
 
 callbacks = (EarlyStopping(monitor='val_loss', patience=20, verbose=1),
                 ReduceLROnPlateau(monitor='val_loss', factor=0.25, patience=10, verbose=1))
-hist = model.fit(train, epochs=100, validation_data=val, callbacks=callbacks)
 
-fig = plt.figure()
+start=time.time()
+history = model.fit(train, batch_size=batch_size , epochs=500, validation_data=val, callbacks=callbacks)
+
+""" fig = plt.figure()
 plt.plot(hist.history['loss'], color='teal', label='loss')
 plt.plot(hist.history['val_loss'], color='orange', label='val_loss')
 fig.suptitle('Loss', fontsize=20)
-plt.legend(loc="upper right")
+plt.legend(loc="upper right") """
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(1, len(acc)+1)
+#Train and validation accuracy 
+plt.figure(figsize=(15, 15))
+plt.subplot(2, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+#Train and validation loss 
+plt.subplot(2, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+print(f'Elapsed time: {time.time()- start}')
 plt.show()
 
-print(model.evaluate(test))
+print(f'test accuracy: {round(model.evaluate(test)[1],3)}')
