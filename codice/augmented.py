1aee2e02158b67b70efa13922d8abbe4c0259fa9
depoale <94780408+depoale@users.noreply.github.com>
2023-01-24 12:50:39 +0100

    augmentation not working

--- codice/augmented.py
+++ codice/augmented.py
@@ -5,45 +5,37 @@
 
 from keras.preprocessing.image import ImageDataGenerator
 from matplotlib import pyplot as plt
-from utils import get_data, plot, callbacks, split
+from utils import read_imgs, plot, callbacks, split
 from models import get_model, get_bigger_model, cnn_model, cnn_classifier
 from models import train_path, test_path, img_height, img_width
-train, val, test = get_data(train_path='new_data/Train', test_path='new_data/Test')
-train_path = 'new_data/Train'
-batch_size=128
-train_datagen = ImageDataGenerator(
-        rotation_range=0,
-        #width_shift_range=0.1,
-        #height_shift_range=0.1,
+from kfold import fold, fold_tuner
+from sklearn.utils import shuffle
+import os
+batch_size=64
+IMGS_DIR ='total_data'
+
+datagen = ImageDataGenerator(
+        rotation_range=10,
+        width_shift_range=3,
+        height_shift_range=3,
         rescale=1./255,
-        #shear_range=0.2,
-        #zoom_range=0.18,
         horizontal_flip=True,
         vertical_flip=True,
-        #fill_mode='reflect', #  nearest?
-        validation_split=split)
+        fill_mode='reflect', #  nearest?
+        validation_split=0)
         
-train_gen = train_datagen.flow_from_directory(
-    train_path,
-    target_size=(img_width, img_height),
-    batch_size=1,
-    color_mode='grayscale', 
-    class_mode='binary',
-    #save_to_dir='augmented',
-    subset='training')
-
-val_gen = train_datagen.flow_from_directory(
-    train_path,
-    target_size=(img_width, img_height),
-    batch_size=1,
-    color_mode='grayscale',
-    class_mode='binary',
-    subset='validation')
+labels= ['0','1']
 
 
 if __name__=='__main__': 
-    model = cnn_classifier()
-    history = model.fit(train_gen, batch_size=1 , epochs=100, validation_data=val_gen, callbacks=callbacks)
-    plot(history=history)
-    print(f'test accuracy: {round(model.evaluate(test)[1],3)}') 
-    print(model.evaluate(test))
+    for label in labels:
+        path = os.path.join(os.getcwd(),IMGS_DIR)
+        print(path)
+        dir_path = os.path.join(os.getcwd(),'augmented_data')
+        print(dir_path)
+        new_imgs = datagen.flow_from_directory(
+                path,
+                target_size=(img_width, img_height),
+                batch_size=1,
+                color_mode='grayscale', 
+                save_to_dir=dir_path)
