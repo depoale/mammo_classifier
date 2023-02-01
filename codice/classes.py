1c3b6f374f2a4435f27eb95da3873a2c6dbe4601
francescoleonetti <francescoleonetti1234@gmail.com>
2023-02-01 11:08:11 +0100

    Fra ripristinato cose varie

--- codice/classes.py
+++ codice/classes.py
@@ -1,6 +1,11 @@
 from utils import read_imgs
 import os
 from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import image as img
+import matlab.engine
+from PIL import Image
 
 img_width = 60
 img_height = 60
@@ -18,14 +23,14 @@ class Data():
         
         self._PATH = 'total_data'
         
-        
-        if wavelet:
-            # create wavelet directory and set _PATH to that directory
-            pass
         if augmented:
             # augment data found in _PATH and set _PATH to that directory
             self.aug()
-            
+        
+        if wavelet:
+            # create wavelet directory and set _PATH to that directory
+            self.wave()
+        
         self.set_data(self._PATH)
     
     def set_data(self, directory):
@@ -59,4 +64,78 @@ class Data():
             for k in range(len(gen)):
                 gen.next()
 
+        self._PATH = IMGS_DIR
+
+    
+    def wave(self):
+        eng = matlab.engine.start_matlab()
+
+        Q = 1 #quante dev std considero nel filtraggio (al massimo due consigliate)
+        wave = 'sym3' #quale wavelet utilizzo (anche un altro paio)
+        N = 3 #livello di approssimazione (consigliato 3)
+
+        IMGS_DIR = 'wavelet_data'
+
+        os.makedirs(os.path.join(f'{IMGS_DIR}', '0'))
+        os.makedirs(os.path.join(f'{IMGS_DIR}', '1'))
+
+        dataset_path_0 = os.path.join(f'{self._PATH}', '0')
+        dataset_path_1 = os.path.join(f'{self._PATH}', '1')
+        dataset_paths = [dataset_path_0, dataset_path_1]
+
+        names_0 = os.listdir(dataset_path_0)
+        names_1 = os.listdir(dataset_path_1)
+        names = [names_0, names_1]
+
+        for i in range(0,2,1):
+            for name in names[i]:
+                I = eng.imread(os.path.join(dataset_paths[i], f'{name}'))
+                I = np.asarray(I)
+                
+                c, s = eng.wavedec2(I, N, wave, nargout=2)
+                s = np.asarray(s)
+                c = np.asarray(c)
+                c = c[0]
+
+                A1 = eng.appcoef2(c,s,wave,1, nargout=1)
+                H1,V1,D1 = eng.detcoef2('all',c,s,1, nargout=3)
+
+                A2 = eng.appcoef2(c,s,wave,2, nargout=1)
+                H2,V2,D2 = eng.detcoef2('all',c,s,2, nargout=3)
+
+                A3 = eng.appcoef2(c,s,wave,3, nargout=1)
+                H3,V3,D3 = eng.detcoef2('all',c,s,3, nargout=3)
+
+                size_CM = eng.prod(s,2, nargout=1)
+                size_CM = np.asarray(size_CM)
+
+                c_labels= eng.zeros(1,size_CM[0])
+                c_labels = np.asarray(c_labels)
+                c_labels = c_labels[0]
+
+                for il in range(1, N+1):
+                    ones = eng.ones(1,3*np.double(size_CM[il]))
+                    ones = np.asarray(ones)
+                    ones = ones[0]
+                    c_labels = np.concatenate((c_labels, np.double(N+1-il)*ones))
+
+                std1=np.double(eng.std(c[c_labels==1], nargout=1))
+                std2=np.double(eng.std(c[c_labels==2], nargout=1))
+                std3=np.double(eng.std(c[c_labels==3], nargout=1))
+
+                c_mod = c.copy()
+                c_mod.setflags(write=1)
+                c_mod[c_labels==0]=0
+
+                c_mod[(c_labels==1)&(abs(c)<Q*std1)]=0
+                c_mod[(c_labels==2)&(abs(c)<Q*std2)]=0
+                c_mod[(c_labels==3)&(abs(c)<Q*std3)]=0
+
+                I_rec = eng.waverec2(c_mod,s,wave, nargout=1)
+                I_rec = np.asarray(I_rec)
+
+                plt.imsave(os.path.join(f'{IMGS_DIR}', f'{i}', f'{name}.png'), I_rec, cmap='gray', format='png')
+                Image.open(os.path.join(f'{IMGS_DIR}', f'{i}', f'{name}.png')).convert('L').save(os.path.join(f'{IMGS_DIR}', f'{i}', f'{name}.png'))
+
+        
         self._PATH = IMGS_DIR
--- codice/kfold.py
+++ codice/kfold.py
@@ -1,5 +1,5 @@
 import os
-
+import numpy as np
 from matplotlib import pyplot as plt
 import keras_tuner as kt
 
@@ -30,8 +30,13 @@ def fold_tuner(X, y, k, modelBuilder, overwrite=True):
         Returs best hps list (one entry for each fold)"""
     test_acc=[]
     best_hps_list=[]
+    
     plt.figure('ROC - Testing')
     plt.title('ROC - Testing')
+    tprs = []
+    aucs = []
+    mean_fpr = np.linspace(0, 1, 100)
+    
     plt.figure('Confusion Matrices')
 
     colors = ['green', 'red', 'blue', 'darkorange', 'gold']
@@ -57,10 +62,25 @@ def fold_tuner(X, y, k, modelBuilder, overwrite=True):
         test_acc.append(accuracy)
         
         plot(history=history, i=i)
-        ROC(X_test, y_test=y_test, model=best_model, color=colors[i], i=i+1)
+        ROC(X_test, y_test=y_test, model=best_model, color=colors[i], i=i+1, mean_fpr=mean_fpr, tprs=tprs, aucs=aucs)
         get_confusion_matrix(X_test, y_test=y_test, model=best_model, i=i+1)
 
-    print(test_acc)
+    
+    mean_tpr = np.mean(tprs, axis=0)
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    std_auc = np.std(aucs)
+    plt.figure('ROC - Testing')
+    plt.plot(mean_fpr, mean_tpr, color="black", label=r"Mean ROC (AUC = %0.2f $\pm$ %0.2f)" % (mean_auc, std_auc), lw=2, alpha=0.8)
+    
+    std_tpr = np.std(tprs, axis=0)
+    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+    plt.figure('ROC - Testing')
+    plt.fill_between(mean_fpr, tprs_lower, tprs_upper, color="grey", alpha=0.2, label=r"$\pm$ 1 std. dev.")
+    plt.legend(loc='lower right')
+    
+    print(f'Test Accuracy {test_acc}')
     print(f'Expected accuracy: {round(stats.mean(test_acc),3)}+/- {round(stats.stdev(test_acc),3)}')
     print(f'best hps:{best_hps_list}')
     plt.show()
