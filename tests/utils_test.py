ac9a446104d37bc65e4af8485bcb993c1a6ef5bd
depoale <94780408+depoale@users.noreply.github.com>
2023-02-12 12:14:32 +0100

    ops

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -10,23 +10,23 @@ import utils
 class UtilsTests(unittest.TestCase):
     def test_read_imgs(self):
         X, y = utils.read_imgs('total_data', [0, 1])
-        assert(len(X)==len(y))
+        self.assertEqual(len(X)==len(y))
 
     def test_str2bool(self):
-        assert(utils.str2bool('YES')== True)
-        assert(utils.str2bool('y')== True)
-        assert(utils.str2bool('0')== False)
-        assert(utils.str2bool('F')== False)
+        self.assertEqual(utils.str2bool('YES')== True)
+        self.assertEqual(utils.str2bool('y')== True)
+        self.assertEqual(utils.str2bool('0')== False)
+        self.assertEqual(utils.str2bool('F')== False)
     
     def test_nearest_square(self):
-        assert(utils.nearest_square(16)== 4)
-        assert(utils.nearest_square(12)== 3)
-        assert(utils.nearest_square(6)== 2)
+        self.assertEqual(utils.nearest_square(16)== 4)
+        self.assertEqual(utils.nearest_square(12)== 3)
+        self.assertEqual(utils.nearest_square(6)== 2)
     
     def test_get_rows_columns(self):
-        assert(utils.get_rows_columns(16)== (4,4))
-        assert(utils.get_rows_columns(17)== (4,5))
-        assert(utils.get_rows_columns(12)== (3,4))
+        self.assertEqual(utils.get_rows_columns(16)== (4,4))
+        self.assertEqual(utils.get_rows_columns(17)== (4,5))
+        self.assertEqual(utils.get_rows_columns(12)== (3,4))
         
         
 
