06b9fb338451eba53578fbffd38bf68711d3c683
francescoleonetti <francescoleonetti1234@gmail.com>
2023-02-15 10:38:43 +0100

    aaaaaaaaa

--- tests/tools_test.py
+++ tests/tools_test.py
@@ -6,8 +6,8 @@ import sys
 import torch
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "codice")))
-from classes import Data, Model
-from utils import delete_directory
+#from classes import Data, Model
+#from utils import delete_directory
 import tools_for_Pytorch as tools
 
 
